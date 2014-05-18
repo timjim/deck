d36b46a8f9d3e4224297828963531ae45d6c1294
Tim Elder <timothy.j.elder@gmail.com>
2014-05-18 14:48:16 +0100

    Link stylesheets, reference JS in HTML head, insert Campaign Monitor HTML, split Campaign Monitor JS to a standalone file

---
+++ assets/javascript/campaignMonitor.js
@@ -0,0 +1,11 @@
+function campaignMonitor () {
+  var e = document.createElement('script');
+  e.type = 'text/javascript';
+  e.async = true;
+  e.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://btn.createsend1.com/js/sb.min.js?v=2';
+  e.className = 'createsend-script';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(e, s);
+})
+
+campaignMonitor();
