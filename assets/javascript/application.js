ac61dbac9fed4fa5004608ee0303799e105571ed
Tim Elder <timothy.j.elder@gmail.com>
2014-05-19 19:46:10 +0100

    Link to YouTube for iOS devices

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,11 +1,12 @@
 $(function() {
   
-  // Lightbox fadeIn
-  $('#how_it_works').click(function() {
-    $('#lightbox').fadeIn();
-    // Autoplay
-    $('#video')[0].src += "&autoplay=1";
-  });
+  function desktop() {
+    $('#how_it_works').click(function() {
+      $('#lightbox').fadeIn();
+      // Autoplay
+      $('#video')[0].src += "&autoplay=1";
+    });
+  }
 
   // Lightbox fadeOut
   $('#lightbox').click(function() {
@@ -20,5 +21,14 @@ $(function() {
 
   // Responsive iFrame
   $('#video_container').fitVids();
+
+  // Check if device is iOS
+  if( /iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
+    // Open with native app
+    $("#how_it_works").wrap("<a href='http://www.youtube.com/watch?v=frfUORPk2O0' style='text-decoration:none;'></a>");
+  } else {
+    // Fade in lightbox
+    desktop();
+  }
 
 });
